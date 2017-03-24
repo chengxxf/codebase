875f1af741ddba5aa88e731b0d441312bf14b07c
chengxxf <chengxyd@163.com>
2017-03-24 16:31:28 +0800

    multiply

---
+++ operator/multiply_bytes.cpp
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include <sys/types.h>
+
+#include <list>
+
+
+int multiply_bytes10(u_char *a,int aLen,u_char *b,int bLen, u_char **ppRes,int *pOutLen)
+{
+	if(ppRes==NULL || pOutLen==NULL ) return -1;
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
